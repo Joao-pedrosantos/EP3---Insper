c6119c462e35003aadcb8d03c28c72f7ba998724
Luiza <luizafe@al.insper.edu.br>
2021-05-24 17:50:34 -0300

    classe de carrinhos arrumada

--- carrinhos.py
+++ carrinhos.py
@@ -4,17 +4,26 @@ class carros:
         car = ['frogger/assets/img/carro_verd.png', 'frogger/assets/img/carro_azul.png', 'frogger/assets/img/carro_verm.png', 'frogger/assets/img/carro_especial.png']
         sorteio = randint(0,len(car)-1)
         sapo_img = pygame.image.load(car[sorteio]).convert_alpha()
-
+        lado = randint(0,1)
         pygame.sprite.Sprite.__init__(self)
-
-        self.image = assets['carro_especial.png']
-        self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 10
-        self.speedx = 10
-        self.groups = groups
-        self.assets = assets
+        
+        if lado == 0:
+            self.image = assets[car[sorteio]]
+            self.rect = self.image.get_rect()
+            self.rect.centerx = WIDTH / 2
+            self.rect.bottom = HEIGHT - 10
+            self.speedx = 10
+            self.groups = groups
+            self.assets = assets
+        else:
+            self.image = assets[car[sorteio].pygame.transform.flip()]
+            self.rect = self.image.get_rect()
+            self.rect.centerx = WIDTH / 2
+            self.rect.bottom = HEIGHT - 10
+            self.speedx = 10
+            self.groups = groups
+            self.assets = assets
 
         if car[sorteio] == 'frogger/assets/img/carro_especial.png':
             self.speedx = 20
-            
+
