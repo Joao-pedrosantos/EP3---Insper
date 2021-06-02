a6c47cdc35eb22be4303e3d8dcd769e6cdd581df
Joao Pedro Santos <maxxjotinha@gmail.com>
2021-06-01 23:42:05 -0300

    jogo mais atualizado com musiquinha nova

--- frogger_v4.py
+++ frogger_v4.py
@@ -2,6 +2,8 @@ from typing import get_origin
 import pygame
 from random import *
 from pygame import mixer
+
+
 pygame.init()
 pygame.mixer.init()
 
@@ -14,8 +16,6 @@ carro_height = 80
 
 WIDTH = 750
 HEIGHT = 1000
-#linhas = HEIGHT/(sapo_height/2)
-#colunas = WIDTH/(sapo_width/2)
 
 
 
@@ -28,7 +28,7 @@ class Carros(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         
         self.image = assets['Carros'] [randint(0,len(assets['Carros'])-1)]
-
+        self.id = randint(1,100000)
         self.rect = self.image.get_rect()
         self.rect.x = assets['Spawnx'] [randint(0,len(assets["Spawnx"])-1)]
         self.rect.y = assets['Spawny'] [randint(0,len(assets["Spawny"])-1)]
@@ -61,56 +61,31 @@ class Galinha(pygame.sprite.Sprite):
         self.vy = 0
         self.vx = 0
         self.assets = assets
+        self.morta = 0
 
-
+    def matar(self):
+        self.morta = 150
+    
     def update(self):
-        self.rect.x += self.vx//3
-        self.rect.y -= -(self.vy/3)
-        if self.vx > 0:
-            self.image = pygame.transform.flip(sapo_img_small, False, False)
-        elif self.vx < 0:
-            self.image = pygame.transform.flip(sapo_img_small, True, False)
+        if self.morta <= 0:    
+            self.rect.x += self.vx//3
+            self.rect.y -= -(self.vy/3)
+            
+            if self.vx > 0:
+                self.image = pygame.transform.flip(sapo_img_small, False, False)
+            elif self.vx < 0:
+                self.image = pygame.transform.flip(sapo_img_small, True, False)
 
-        if self.rect.bottom > HEIGHT:
-            self.rect.bottom = HEIGHT 
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
-        if self.rect.left < 0:
-            self.rect.left = 0
-
-""" class Pedra(pygame.sprite.Sprite):
-    
-    
-    def __init__(self, groups, assets):
-        # Construtor da classe mãe (Sprite).
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = assets['pedra.png']
-        self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 10
-        self.speedx = 0
-        self.groups = groups
-        self.assets = assets
-
-class tronco(pygame.sprite.Sprite):
-    
-    
-    def __init__(self, groups, assets):
-        # Construtor da classe mãe (Sprite).
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = assets['tronco.png']
-        self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 10
-        self.speedx = 0
-        self.groups = groups
-        self.assets = assets
-    
- """
-
-
+            if self.rect.bottom > HEIGHT:
+                self.rect.bottom = HEIGHT 
+            if self.rect.right > WIDTH:
+                self.rect.right = WIDTH
+            if self.rect.left < 0:
+                self.rect.left = 0
+        else:
+            self.morta -= 1
+            self.rect.centerx = WIDTH / 2
+            self.rect.bottom = HEIGHT - 10
 
 
 font = pygame.font.SysFont(None, 48)
@@ -161,8 +136,11 @@ assets['Spawnx'] = [
 800    
 ]
 
+assets['sound effects'] = [
+    
+]
 mixer.music.load('frogger/musica/Pitfall.mp3')
-mixer.music.set_volume(0.2)
+mixer.music.set_volume(0.01)
 mixer.music.play(-1)
 
 
@@ -206,17 +184,18 @@ font = pygame.font.SysFont(None, 48)
 
 keys_down = {}
 
-lives = 3
+
 
 tempo_vivo = 0
 tempo_em_s = 0
-
+nivel1 = 1
+dificuldade = 1
 #comeca jogo
 while game:
-    nivel1 = 1
+    
     tempo_v = font.render('{0}s'.format(tempo_em_s), True, (255, 255, 255))
     nivel = font.render('Nível {0}'.format(nivel1), True, (255,0,0))
-    t = 150
+    
     if tempo_vivo == FPS:
         tempo_em_s += 1
         tempo_vivo = 0
@@ -224,19 +203,15 @@ while game:
     clock.tick(FPS)
 
     # Verifica se houve colisão entre galinha e carro
-    hits = pygame.sprite.spritecollide(player, all_carros, True)
+   
 
-    if len(hits) > 0:
-        player.rect.centerx = WIDTH / 2
-        player.rect.bottom = HEIGHT - 10
-        t = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
         # Verifica se apertou alguma tecla.
     
-        if event.type == pygame.KEYDOWN:# and t > 150:
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 player.vx -= sapo_width/2
 
@@ -247,11 +222,9 @@ while game:
                 player.vy -= sapo_height/2
             if event.key == pygame.K_DOWN:
                 player.vy += sapo_height/2
-            if event.type == pygame.K_q:
-                game = False
     
         # Verifica se soltou alguma tecla.
-        if event.type == pygame.KEYUP: #and t > 150:
+        if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 player.vx = 0
             if event.key == pygame.K_RIGHT:
@@ -260,23 +233,53 @@ while game:
                 player.vy = 0 
             if event.key == pygame.K_DOWN:
                 player.vy = 0 
-
-    while len(all_carros) < 10:
+    
+    if dificuldade == 1:
+        difc = 5
+    else:
+        difc = 5 + dificuldade*5
+    
+    while len(all_carros) < difc:
         carrinho = Carros(assets)
         all_carros.add(carrinho)
         all_sprites.add(carrinho)
 
-    t += 1
+
+    if player.rect.top <= 0:
+        dificuldade += 1
+        nivel1 += 1
+        player.rect.centerx = WIDTH / 2
+        player.rect.bottom = HEIGHT - 10
     # ----- Atualiza estado do jogo
     
     # Atualizando a posição do meteoro
     window.fill((0, 0, 0))  # Preenche com a cor branca
     window.blit(background, (0, 0))
-    #window.blit(player, (sapo_x, sapo_y))
-    
+  
     all_sprites.update()
+
+
+    hit = pygame.sprite.spritecollide(player, all_carros, True)
+
+    if len(hit) > 0:
+        player.matar()
+        mixer.music.load('frogger/musica/mf.mp3')
+        mixer.music.set_volume(0.1)
+        mixer.music.play()
+    
+    """ for carro in all_carros:    
+        hits = pygame.sprite.spritecollide(carro, all_carros, True)
+        for hit in hits:
+            if carro.id != hit.id:
+                carro.speedx = 5
+                hit.speedx = 5
+ """
+   
+    
     all_sprites.draw(window)
- 
+    
+
+
   
     window.blit(tempo_v, (0,0))
     window.blit(nivel, (0,30))  
